9a3b9f55a4271bdfccee09ab0739c2fb7251819e
Bruno Sousa <bbarros.sousa@hotmail.com>
2020-10-02 19:39:02 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,111 +2,100 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <stdint.h>
+#include <inttypes.h>
 #include "estruturas.h"
-
 
 //esse eh o arquivo que iremos escrever
 //o deixamos como variavel global
 FILE *file_out;
 
+//também deixaremos as tabelas aleatórias como variáveis globais
+uint64_t tabelasAleatorias[8][0x100];
 
+struct pos_tabela {
+  uint64_t chave;
+  //'0' se posição for vazia, '1' se for liberada, '2' se for ocupada
+  unsigned int flag : 2;
+};
 
 struct hashtable {
   //consiste em um vetor de inteiros
-  //conterá '0' se uma posição for vazia e '2^64-1' (-1) se ela for liberada
-  unsigned long long *tabela;
+  posTabela *tabela;
 
   //o número de elementos suportado pela tabela
-  unsigned long long tamanho;
+  uint64_t tamanho;
 
   //precisaremos desse valor para implementarmos a operacao de "limpar"
-  unsigned long long numeroDeCodigosDeRemocao;
-
-  unsigned long long tabelasAleatorias[8][0x100];
+  uint64_t numeroDeCodigosDeRemocao;
 };
 
 //hashtable_dinamica implementa table doubling/halving
+//ela também contém um ponteiro para as tabelas aleatórias
 struct hashtable_dinamica {
-  unsigned long long tamanho;
-  unsigned long long numeroElementos;
+  uint64_t tamanho;
+  uint64_t numeroElementos;
 
   HashTable *hashtable;
 };
 
 
 
+
 //Funções da tabela de dispersão normal
 
-//Variavel global para passarmos como 'seed' e incrementarmos cada vez que usamos a função 'rand()'
-unsigned long long num = 1;
-
-//Preenche os valores da tabela aleatoria a partir do numero 'valoresPossiveis', que eh o número de valores em que cada chave pode ser mapeada (o tamanho da nossa tabela de dispersao)
-void preencheTabelaAleatoria (unsigned long long tabelaAleatoria[0x100], unsigned long long valoresPossiveis) {
-
-  srand(num);
-  num++;
-
-  //a tabelaAleatoria tem 2^8 posicoes
-  for (unsigned long long i = 0; i < 0x100; i++) {
-    tabelaAleatoria[i] = (rand())%valoresPossiveis;
-  }
-
-}
-
-HashTable *criaHashTable (unsigned long long tamanho) {
+HashTable *criaHashTable (uint64_t tamanho) {
   //'tamanho' é sempre uma potência de 2, o que enforçamos nas funções de HashTableDinamica 
 
   HashTable *H = malloc(sizeof(HashTable));
 
-  H->tabela = malloc(tamanho*sizeof(unsigned long long));
-  for (unsigned long long i = 0; i < tamanho; i++) {
-    (H->tabela)[i] = 0;
+  H->tabela = malloc(tamanho*sizeof(posTabela));
+  for (uint64_t i = 0; i < tamanho; i++) {
+    (H->tabela)[i].chave = 0;
+    (H->tabela)[i].flag = 0;
   }
 
   H->tamanho = tamanho;
-
-  for (unsigned long long i = 0; i < 8; i++) {
-    preencheTabelaAleatoria (H->tabelasAleatorias[i], H->tamanho);
-  }
 
   return H;
 }
 
 //a nossa função de dispersão
-unsigned long long funcHashTable (HashTable *H, unsigned long long chave) {
+uint64_t funcHashTable (uint64_t chave) {
 
-  unsigned long long valor = 0;
+  uint64_t valor = 0;
 
-  for (unsigned long long i = 0; i < 8; i++) {
+  for (uint64_t i = 0; i < 8; i++) {
     //a cada iteração fazemos um 'XOR' entre o valor anterior e o valor a que a tabela mapeia um determinado grupo de dígitos de 'chave'
-    valor = valor^(H->tabelasAleatorias[i][chave%(0x100)]);
+    valor ^= (tabelasAleatorias[i][chave%(0x100)]);
     chave >>= 8;
   }
   
   return valor;
 }
 
-//retorna a posicao onde o elemento foi encontrado ou '-1' se ele nao for
-unsigned long long buscaHashTable (HashTable *H, unsigned long long chave) {
+//retorna '1' em caso de sucesso e '0' caso contrário
+int buscaHashTable (HashTable *H, uint64_t chave) {
 
-  unsigned long long posChave = funcHashTable(H, chave);
-  unsigned long long posChaveInicial = posChave;
+  uint64_t funcChave = funcHashTable(chave);
+  uint64_t posChave = funcChave%(H->tamanho);
+  uint64_t posChaveInicial = posChave;
 
   while (posChave < H->tamanho) {
     //'0' indica que espaço é vazio
-    if (H->tabela[posChave] == 0) {
+    if (H->tabela[posChave].flag == 0) {
 
       //adicionando ao arquivo de texto
-      fprintf(file_out, "%lld %d\n", posChaveInicial, -1);
+      fprintf(file_out, "%"PRIu64" %d\n", funcChave, -1);
 
-      return -1;
+      return 0;
     }
-    if (H->tabela[posChave] == chave) {
+    if ( (H->tabela[posChave].flag == 2) && (H->tabela[posChave].chave == chave) ) {
 
       //adicionando ao arquivo de texto
-      fprintf(file_out, "%lld %lld\n", posChaveInicial, posChave);
+      fprintf(file_out, "%"PRIu64" %"PRIu64"\n", funcChave, posChave);
 
-      return posChave;
+      return 1;
     }
     posChave += 1;
   }
@@ -116,41 +105,49 @@ unsigned long long buscaHashTable (HashTable *H, unsigned long long chave) {
   
   while (posChave < posChaveInicial) {
     //'0' indica que espaço é vazio
-    if (H->tabela[posChave] == 0) {
+    if (H->tabela[posChave].flag == 0) {
 
       //adicionando ao arquivo de texto
-      fprintf(file_out, "%lld %d\n", posChaveInicial, -1);
+      fprintf(file_out, "%"PRIu64" %d\n", funcChave, -1);
 
-      return -1;
+      return 0;
     }
-    if (H->tabela[posChave] == chave) {
+    if ( (H->tabela[posChave].flag == 2) && (H->tabela[posChave].chave == chave) ) {
 
       //adicionando ao arquivo de texto
-      fprintf(file_out, "%lld %lld\n", posChaveInicial, posChave);
+      fprintf(file_out, "%"PRIu64" %"PRIu64"\n", funcChave, posChave);
 
-      return posChave;
+      return 1;
     }
     posChave += 1;
   }
 
   //adicionando ao arquivo de texto
-  fprintf(file_out, "%lld %d\n", posChaveInicial, -1);
+  fprintf(file_out, "%"PRIu64" %d\n", funcChave, -1);
 
-  return -1;
+  return 0;
 }
 
-//retorna a posicao onde o elemento foi inserido na tabela
-unsigned long long insereHashTable (HashTable *H, unsigned long long chave) {
+//retorna '1' em caso de sucesso e '0' caso contrário
+//'imprimir' indica se devemos escrever algo no arquivo de texto
+int insereHashTable (HashTable *H, uint64_t chave, int imprimir) {
 
-  unsigned long long posChave = funcHashTable(H, chave);
-  unsigned long long posChaveInicial = posChave;
+  uint64_t funcChave = funcHashTable(chave);
+  uint64_t posChave = funcChave%(H->tamanho);
+  uint64_t posChaveInicial = posChave;
 
   while (posChave < H->tamanho) {
-    //'0' indica que o espaço é vazio e '-1' (2^64-1) que é liberado
-    if ( (H->tabela[posChave] == 0) || (H->tabela[posChave] == -1) ) {
-      H->tabela[posChave] = chave;
+    //flag diferente de '2' significa que a posicao é vazia ou liberada
+    if ( H->tabela[posChave].flag != 2 ) {
+      H->tabela[posChave].chave = chave;
+      H->tabela[posChave].flag = 2;
 
-      return posChave;
+      if (imprimir) {
+        //adicionando ao arquivo de texto
+        fprintf(file_out, "%"PRIu64" %"PRIu64"\n", funcChave, posChave);
+      }
+
+      return 1;
     }
     posChave +=1;
   }
@@ -159,40 +156,51 @@ unsigned long long insereHashTable (HashTable *H, unsigned long long chave) {
   posChave = 0;
 
   while (posChave < posChaveInicial) {
-    //'0' indica que o espaço é vazio e '-1' (2^64-1) que é liberado
-    if ( (H->tabela[posChave] == 0) || (H->tabela[posChave] == -1) ) {
-      H->tabela[posChave] = chave;
+    //flag diferente de '2' significa que a posicao é vazia ou liberada
+    if ( H->tabela[posChave].flag != 2 ) {
+      H->tabela[posChave].chave = chave;
+      H->tabela[posChave].flag = 2;
 
-      return posChave;
+      if (imprimir) {
+        //adicionando ao arquivo de texto
+        fprintf(file_out, "%"PRIu64" %"PRIu64"\n", funcChave, posChave);
+      }
+
+      return 1;
     }
     posChave += 1;
   }
 
   //essa linha em teoria nao deve ser executada
-  printf("Oh no!");
-  return -1;
+  if (imprimir) {
+    //adicionando ao arquivo de texto
+    fprintf(file_out, "%"PRIu64" %d\n", funcChave, -1);
+  }
+
+  return 0;
 }
 
 //limpamos os codigos de remocao simplesmente reconstruindo a tabela
 void limpaCodigosRemocao (HashTable *H) {
 
-  unsigned long long* tabelaAuxiliar = malloc(sizeof(unsigned long long)*H->tamanho); 
+  uint64_t* tabelaAuxiliar = malloc(sizeof(uint64_t)*H->tamanho); 
 
-  unsigned long long cont = 0;
-  for (unsigned long long i = 0; i < H->tamanho; i++) {
+  uint64_t cont = 0;
+  for (uint64_t i = 0; i < H->tamanho; i++) {
 
-    if ( (H->tabela[i] != 0) && (H->tabela[i] != -1) ) {
+    if (H->tabela[i].flag == 2) {
       //Adicionando todas as chaves a tabela auxiliar
-      tabelaAuxiliar[cont] = H->tabela[i];
+      tabelaAuxiliar[cont] = H->tabela[i].chave;
       cont++;
     }
-    H->tabela[i] = 0;
+    H->tabela[i].chave = 0;
+    H->tabela[i].flag = 0;
 
   }
 
-  for (unsigned long long i = 0; i < cont; i++) {
+  for (uint64_t i = 0; i < cont; i++) {
     //Reinserimos todas as chaves
-    insereHashTable(H, tabelaAuxiliar[i]);
+    insereHashTable(H, tabelaAuxiliar[i], 0);
   }
 
   H->numeroDeCodigosDeRemocao = 0;
@@ -204,34 +212,36 @@ void limpaCodigosRemocao (HashTable *H) {
 
 }
 
-//retorna a posicao em que o elemento foi removido ou '-1' se nenhum elemento foi removido
-unsigned long long removeHashTable (HashTable *H, unsigned long long chave) {
+//retorna '1' em caso de sucesso e '0' caso contrário
+uint64_t removeHashTable (HashTable *H, uint64_t chave) {
   
-  unsigned long long posChave = funcHashTable(H, chave);
-  unsigned long long posChaveInicial = posChave;
+  uint64_t funcChave = funcHashTable(chave);
+  uint64_t posChave = funcChave%(H->tamanho);
+  uint64_t posChaveInicial = posChave;
 
   while (posChave < H->tamanho) {
     //'0' indica que espaço é vazio
-    if (H->tabela[posChave] == 0) {
+    if (H->tabela[posChave].flag == 0) {
 
       //adicionando ao arquivo de texto
-      fprintf(file_out, "%lld %d\n", posChaveInicial, -1);
+      fprintf(file_out, "%"PRIu64" %d\n", funcChave, -1);
 
-      return -1;
+      return 0;
     }
-    if (H->tabela[posChave] == chave) {
+    if ( (H->tabela[posChave].flag == 2) && (H->tabela[posChave].chave == chave)) {
       //liberamos o espaco
-      H->tabela[posChave] = -1;
+      H->tabela[posChave].flag = 1;
+      H->tabela[posChave].chave = 0;
 
       //adicionando ao arquivo de texto
-      fprintf(file_out, "%lld %lld\n", posChaveInicial, posChave);
+      fprintf(file_out, "%"PRIu64" %"PRIu64"\n", funcChave, posChave);
 
       H->numeroDeCodigosDeRemocao += 1;
       if (H->numeroDeCodigosDeRemocao >= H->tamanho/4) {
         limpaCodigosRemocao(H);
       }
 
-      return posChave;
+      return 1;
     }
     posChave += 1;
   }
@@ -241,31 +251,32 @@ unsigned long long removeHashTable (HashTable *H, unsigned long long chave) {
 
   while (posChave < posChaveInicial) {
     //'0' indica que espaço é vazio
-    if (H->tabela[posChave] == 0) {
+    if (H->tabela[posChave].flag == 0) {
 
       //adicionando ao arquivo de texto
-      fprintf(file_out, "%lld %d\n", posChaveInicial, -1);
+      fprintf(file_out, "%"PRIu64" %d\n", funcChave, -1);
 
-      return -1;
+      return 0;
     }
-    if (H->tabela[posChave] == chave) {
+    if ( (H->tabela[posChave].flag == 2) && (H->tabela[posChave].chave == chave)) {
       //liberamos o espaco
-      H->tabela[posChave] = -1;
+      H->tabela[posChave].flag = 1;
+      H->tabela[posChave].chave = 0;
 
       //adicionando ao arquivo de texto
-      fprintf(file_out, "%lld %lld\n", posChaveInicial, posChave);
+      fprintf(file_out, "%"PRIu64" %"PRIu64"\n", funcChave, posChave);
 
       H->numeroDeCodigosDeRemocao += 1;
       if (H->numeroDeCodigosDeRemocao >= H->tamanho/4) {
         limpaCodigosRemocao(H);
       }
 
-      return posChave;
+      return 1;
     }
     posChave += 1;
   }
 
-  return -1;
+  return 0;
 }
 
 void deletaHashTable (HashTable *H) {
@@ -278,6 +289,32 @@ void deletaHashTable (HashTable *H) {
 
 //Funções da HashTable hashtableDinamica
 
+uint64_t geraNumeroAleatorio () {
+
+  uint64_t randomInt = 0;
+
+  int logRandMax = log2(RAND_MAX)+1;
+  int cont = 64;
+  while (cont >= logRandMax) {
+    cont -= logRandMax;
+    randomInt = (randomInt<<logRandMax) + rand();
+  }
+  randomInt = (randomInt<<cont) + rand()%((int) pow(2,cont));
+
+  return randomInt;
+}
+
+//Preenche os valores da tabela aleatoria a partir do numero 'valoresPossiveis', que eh o número de valores em que cada chave pode ser mapeada (o tamanho da nossa tabela de dispersao)
+
+void preencheTabelaAleatoria (uint64_t tabelaAleatoria[0x100]) {
+
+  //a tabelaAleatoria tem 2^8 posicoes
+  for (uint64_t i = 0; i < 0x100; i++) {
+    tabelaAleatoria[i] = geraNumeroAleatorio();
+  }
+
+}
+
 HashTableDinamica *criaHashTableDinamica () {
   HashTableDinamica *T = (HashTableDinamica *)malloc(sizeof(HashTableDinamica));
 
@@ -285,33 +322,36 @@ HashTableDinamica *criaHashTableDinamica () {
   T->numeroElementos = 0;
   T->hashtable = criaHashTable(4);
 
+  for (uint64_t i = 0; i < 8; i++) {
+    preencheTabelaAleatoria (tabelasAleatorias[i]);
+  }
+
   //adicionando ao arquivo de texto
   fprintf(file_out, "TAM:4\n\n\n");
 
   return T;
 }
 
-//retorna a posicao onde o elemento foi encontrado ou '-1' se ele nao for
-unsigned long long buscaHashTableDinamica (HashTableDinamica *T, unsigned long long chave) {
+//retorna '1' em caso de sucesso e '0' caso contrário
+int buscaHashTableDinamica (HashTableDinamica *T, uint64_t chave) {
+
   return buscaHashTable (T->hashtable, chave);
+
 }
 
-//retorna a posicao onde o elemento foi inserido na tabela
-unsigned long long insereHashTableDinamica (HashTableDinamica *T, unsigned long long chave) {
+//retorna '1' em caso de sucesso e '0' caso contrário
+int long insereHashTableDinamica (HashTableDinamica *T, uint64_t chave) {
 
-  unsigned long long pos = insereHashTable(T->hashtable, chave);
+  int res = insereHashTable(T->hashtable, chave, 1);
   T->numeroElementos +=1;
-
-  //adicionando ao arquivo de texto
-  fprintf(file_out, "%lld %lld\n", funcHashTable(T->hashtable, chave), pos);
 
   //O 'tamanho' sempre será pelo menos o dobro do número de elementos (epsilon = 1, o que significa que o tempo por operação deve ser O(1))
   if (T->tamanho == 2*T->numeroElementos) {
 
     HashTable *H = criaHashTable(2*(T->tamanho));
-    for (unsigned long long i = 0; i < (T->tamanho); i++) {
-      if ( (T->hashtable->tabela[i] != 0) && (T->hashtable->tabela[i] != -1) ) {
-        insereHashTable(H, T->hashtable->tabela[i]);
+    for (uint64_t i = 0; i < (T->tamanho); i++) {
+      if ( T->hashtable->tabela[i].flag == 2 ) {
+        insereHashTable(H, T->hashtable->tabela[i].chave, 0);
       }
     }
     deletaHashTable(T->hashtable);
@@ -319,27 +359,27 @@ unsigned long long insereHashTableDinamica (HashTableDinamica *T, unsigned long 
     T->tamanho = 2*(T->tamanho);
 
     //adicionando ao arquivo de texto
-    fprintf(file_out, "\n\nDOBRAR TAM:%lld\n", T->tamanho);
+    fprintf(file_out, "\n\nDOBRAR TAM:%"PRIu64"\n", T->tamanho);
 
   }
 
-  return pos;
+  return res;
 }
 
-//retorna a posicao em que o elemento foi removido ou '-1' se nenhum elemento foi removido
-unsigned long long removeHashTableDinamica (HashTableDinamica *T, unsigned long long chave) {
+//retorna '1' em caso de sucesso e '0' caso contrário
+int removeHashTableDinamica (HashTableDinamica *T, uint64_t chave) {
 
-  unsigned long long pos = removeHashTable(T->hashtable, chave);
+  int res = removeHashTable(T->hashtable, chave);
 
-  if (pos != -1) {
+  if (res) {
     T->numeroElementos -= 1;
 
     if (T->tamanho == 8*T->numeroElementos) {
 
       HashTable *H = criaHashTable((T->tamanho)/2);
-      for (unsigned long long i = 0; i < (T->tamanho); i++) {
-        if ( (T->hashtable->tabela[i] != 0) && (T->hashtable->tabela[i] != -1) ) {
-          insereHashTable(H, T->hashtable->tabela[i]);
+      for (uint64_t i = 0; i < (T->tamanho); i++) {
+        if ( T->hashtable->tabela[i].flag == 2 ) {
+          insereHashTable(H, T->hashtable->tabela[i].chave, 0);
         }
       }
 
@@ -348,13 +388,13 @@ unsigned long long removeHashTableDinamica (HashTableDinamica *T, unsigned long 
       T->tamanho = (T->tamanho)/2;
       
       //adicionando ao arquivo de texto
-      fprintf(file_out, "\n\nMETADE TAM:%lld\n", T->tamanho);
+      fprintf(file_out, "\n\nMETADE TAM:%"PRIu64"\n", T->tamanho);
 
     }
 
   }
 
-  return pos;
+  return res;
 }
 
 
@@ -381,7 +421,7 @@ int main(int argc, char **argv) {
   HashTableDinamica *T = criaHashTableDinamica();
 
   char line[256];
-  unsigned long long chave;
+  uint64_t chave;
   while (fgets(line, sizeof(line), file_in)) {
 
     fprintf(file_out, "%s", line);
@@ -390,10 +430,6 @@ int main(int argc, char **argv) {
     } 
 
     chave = atoi(line+4);
-    if ( (chave == 0) || (chave == -1) ) {
-      printf("Por favor nao use chaves com valores '0' ou '2^64-1' porque esses valores estao sendo usados como flags.");
-      break;
-    }
 
     switch (line[0]) {
       case 'B': 
